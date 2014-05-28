21cb74b377d3f49abeca5d8fc13eb713950f7dc7
Carlos Santos <geral@nexus-pt.eu>
2014-05-28 21:48:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,18 +27,15 @@
 	 *  abaixo é iniciada a criação do template, com base nós ficheiros html
 	 */
 
-	$head = str_replace(
-		array("{c2r-sitename}", "{c2r-keywords}", "{c2r-description}", "{c2r-analytics}"), 
-		array($configuration["site-name"], $language["system"]["keywords"], $language["system"]["description"], $configuration["analytics"]), 
-		file_get_contents("./templates-e/head.html"));
-    	$template = "<html><head>{c2r-head}</head><body>{c2r-content}</body></html>";
+	$head = file_get_contents("./templates-e/head.html");
+    	$footer = file_get_contents("./templates-e/footer.html");
     
     	include "./pages/includes.php";
 
  	// print website
     	print str_replace(
-	    	array( "{c2r-path}", "{c2r-head}", "{c2r-content}", "{c2r-sitename}", "{c2r-keywords}", "{c2r-description}"),
-	    	array( $configuration["path"], $head, $content, $configruation["site-name"], $language["system"]["keywords"], $language["system"]["description"]),
+	    	array( "{c2r-path}", "{c2r-head}", "{c2r-content}", "{c2r-sitename}", "{c2r-keywords}", "{c2r-description}", "{c2r-analytics}"),
+	    	array( $configuration["path"], $head, $content, $configruation["site-name"], $language["system"]["keywords"], $language["system"]["description"], $configuration["analytics"]),
 	    	$template
 	);
 ?>
