f2ac578874f19714dcb68316430ab1f0ba4674f1
Carlos Santos <geral@nexus-pt.eu>
2014-10-11 01:58:42 +0100

    Update includes.php

--- pages/includes.php
+++ pages/includes.php
@@ -1,8 +1,5 @@
 <?php
 	switch ($pg) {
-		case "home":
-			include "./pages/home.php";
-			break;
-		default: 
-			include "./pages/home.php";
+		case "home": include "./pages/home.php"; break;
+		default: include "./pages/home.php";
 	}
