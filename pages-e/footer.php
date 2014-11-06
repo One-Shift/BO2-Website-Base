48a1463b4e9536e5d3ce8c96cb52237680be84c1
Carlos Santos <carlos@nexus-pt.eu>
2014-11-06 19:05:15 +0000

    array bug

--- pages-e/footer.php
+++ pages-e/footer.php
@@ -2,4 +2,4 @@
 	$page_e_template = file_get_contents("./templates-e/footer.html");
 
 	/* last thing */
-	$footer = str_replace( array(), array($header, $footer), $page_e_template);
+	$footer = str_replace( array(""), array(""), $page_e_template);
--- pages-e/header.php
+++ pages-e/header.php
@@ -2,4 +2,4 @@
 	$page_e_template = file_get_contents("./templates-e/header.html");
 
 	/* last thing */
-	$header = str_replace( array(), array($header, $footer), $page_e_template);
+	$header = str_replace( array(""), array(""), $page_e_template);
