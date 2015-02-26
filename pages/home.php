bca594d6dacf9379369c7d7c006aa53bef535dce
Carlos Santos <carlos@nexus-pt.eu>
2015-02-26 16:58:42 +0000

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,19 @@
 <?php
 
-include "./pages-e/header.php";
-include "./pages-e/footer.php";
+$page_template = file_get_contents("templates/home.html");
 
-$page_template = file_get_contents("./templates/home.html");
+include "pages-e/header.php";
+include "pages-e/footer.php";
 
 /* last thing */
 $template = str_replace(
-        array("{c2r-header}", "{c2r-footer}"), array($header, $footer), $page_template
+    [
+        "{c2r-header}", 
+        "{c2r-footer}"
+    ], 
+    [
+    	$header, 
+    	$footer
+	], 
+	$page_template
 );
