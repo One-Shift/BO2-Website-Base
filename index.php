ec8fc5c87ce0441d7084823ec6de1633987c0e05
Carlos Santos <carlos@nexus-pt.eu>
2015-04-09 16:33:39 +0100

    identation + new array type

--- index.php
+++ index.php
@@ -9,23 +9,23 @@ include "pages/functions.php";
 // controlador de sessão
 if (isset($_COOKIE[$configuration["cookie"]]) && !empty($_COOKIE[$configuration["cookie"]])) {
 
-    $cookie = explode(".", $_COOKIE[$configuration["cookie"]]);
+	$cookie = explode(".", $_COOKIE[$configuration["cookie"]]);
 
-    if (count($cookie) === 2) {
-        $cookie[0] = intval($cookie[0]);
-        $cookie[1] = $mysqli->real_escape_string($cookie[1]);
+	if (count($cookie) === 2) {
+		$cookie[0] = intval($cookie[0]);
+		$cookie[1] = $mysqli->real_escape_string($cookie[1]);
 
-        $query[0] = sprintf("SELECT * FROM %s_users WHERE id = '%s' AND password = '%s' LIMIT %s", $configuration["mysql-prefix"], $cookie[0], $cookie[1], 1);
-        $source[0] = $mysqli->query($query[0]);
-        $nr[0] = $source[0]->num_rows;
+		$query[0] = sprintf("SELECT * FROM %s_users WHERE id = '%s' AND password = '%s' LIMIT %s", $configuration["mysql-prefix"], $cookie[0], $cookie[1], 1);
+		$source[0] = $mysqli->query($query[0]);
+		$nr[0] = $source[0]->num_rows;
 
-        if ($nr[0] === 1) {
-            $auth = true;
-            $authData = $source[0]->fetch_assoc();
-        } else {
-            $auth = false;
-        }
-    }
+		if ($nr[0] === 1) {
+			$auth = true;
+			$authData = $source[0]->fetch_assoc();
+		} else {
+			$auth = false;
+		}
+	}
 } else {
 	$auth = false;
 }
@@ -62,9 +62,9 @@ if (isset($_GET["i"]) && !empty($_GET["i"])) {
 
 // controlador de acção
 if (isset($_GET["a"]) && !empty($_GET["a"])) {
-    $a = $mysqli->real_escape_string($_GET["a"]);
+	$a = $mysqli->real_escape_string($_GET["a"]);
 } else {
-    $a = null;
+	$a = null;
 }
 
 $head = file_get_contents("templates-e/head.html");
@@ -103,7 +103,7 @@ $template = str_replace(
 
 // testint minify system
 if ($configuration["minify"]) {
-    print minifyPage($template);
+	print minifyPage($template);
 } else {
-    print $template;
+	print $template;
 }
