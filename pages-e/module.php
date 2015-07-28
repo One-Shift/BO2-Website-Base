69abfb972ca541c6109871d6cf394d41876de057
Carlos Santos <carlos@nexus-pt.eu>
2015-07-28 15:21:15 +0100

    base of a module

---
+++ pages-e/module.php
@@ -0,0 +1,14 @@
+<?php
+
+$page_e_template = file_get_contents("templates-e/module.html");
+
+/* last thing */
+$page_template = str_replace(
+	[
+		""
+	],
+	[
+		$page_e_template
+	],
+	$page_template
+);
