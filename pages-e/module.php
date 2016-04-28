dd6613b01ab19bbbc93f3cff528f57909fb1839f
Carlos Santos <carlos@nexus-pt.eu>
2016-04-28 09:58:00 +0100

    module update

--- pages-e/module.php
+++ pages-e/module.php
@@ -5,10 +5,18 @@ $page_e_template = file_get_contents("templates-e/module.html");
 /* last thing */
 $page_template = str_replace(
 	[
-		""
+		"{c2r-mod-module}"
 	],
 	[
-		$page_e_template
+		str_replace(
+			[
+				""
+			],
+			[
+				""
+			],
+			$page_e_template
+		)
 	],
 	$page_template
 );
